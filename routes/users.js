dd85831f8df1dbe4b9cc8cf0189a65a96c5e031c
Joshua Lehman <joshlehman.dev@gmail.com>
2023-08-02 21:53:34 -0400

    swagger-update

--- routes/users.js
+++ routes/users.js
@@ -4,56 +4,11 @@ const passport = require("passport");
 const catchAsync = require("../utils/catchAsync");
 const users = require("../controllers/users");
 
-/**
- * @swagger
- * /register/:
- *   get:
- *     summary: Register new user form
- *     description: Register new user form
- *     responses:
- *       200:
- *         description: update campground listing contents
- */
-
-
-/**
- * @swagger
- * /register/:
- *   post:
- *     summary: Register new user
- *     description: Register new user
- *     responses:
- *       201:
- *         description: register new user
- */
 router
   .route("/register")
   .get(users.renderRegister)
   .post(catchAsync(users.register));
 
-
-/**
- * @swagger
- * /login/:
- *   get:
- *     summary: login user form
- *     description: login user form
- *     responses:
- *       200:
- *         description: update campground listing contents
- */
-
-
-/**
- * @swagger
- * /login/:
- *   post:
- *     summary: login user
- *     description: login user
- *     responses:
- *       201:
- *         description: login user
- */
 router
   .route("/login")
   .get(users.renderLogin)
@@ -64,18 +19,6 @@ router
     }),
     users.login
   );
-
-
-/**
- * @swagger
- * /logout/:
- *   get:
- *     summary: logout user
- *     description: logout user
- *     responses:
- *       200:
- *         description: update campground listing contents
- */
 
 router.get("/logout", users.logout);
 
