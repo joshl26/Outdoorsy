5450909e75716be0652a29289582c8ad45cd459a
Joshua Lehman <joshlehman.dev@gmail.com>
2024-06-22 08:18:43 -0400

    updates

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.use(methodOverride("_method"));
 //  configures Express to serve static files from the public directory. This allows users to access
 //  files such as images, CSS and JavaScript that are stored in this directory when they visit the
 //  website.
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 //  using the mongoSanitize middleware to protect against malicious MongoDB operations. The
 //  "replaceWith" option specifies what character should replace any potentially unsafe characters,
@@ -173,6 +173,7 @@ const styleSrcUrls = [
 ];
 const connectSrcUrls = [
   "localhost:3001/outdoorsy/",
+  "localhost:3001/outdoorsy/public/",
   "https://joshlehman.ca/outdoorsy/",
   "https://api.mapbox.com/",
   "https://a.tiles.mapbox.com/",
--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -30,9 +30,9 @@ map.on("load", function () {
     paint: {
       // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
       // with three steps to implement three types of circles:
-      //   * Blue, 20px circles when point count is less than 100
-      //   * Yellow, 30px circles when point count is between 100 and 750
-      //   * Pink, 40px circles when point count is greater than or equal to 750
+      //    Blue, 20px circles when point count is less than 100
+      //    Yellow, 30px circles when point count is between 100 and 750
+      //    Pink, 40px circles when point count is greater than or equal to 750
       "circle-color": [
         "step",
         ["get", "point_count"],
